6159bbfef1cc7b1f4e7497ea1b12df9f7ae31139
zavanton123 <zavanton123@gmail.com>
2021-08-18 10:52:42 +0300

    some initial data is added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+insert into company (name)
+values ('Facebook');
+insert into company (name)
+values ('Amazon');
+insert into company (name)
+values ('Apple');
+insert into company (name)
+values ('Netflix');
+insert into company (name)
+values ('Google');
--- src/test/kotlin/com/zavanton/company/repository/CompanyRepositoryTest.kt
+++ src/test/kotlin/com/zavanton/company/repository/CompanyRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.zavanton.company.entity.Company
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -18,10 +19,17 @@ internal class CompanyRepositoryTest {
     @Autowired
     lateinit var companyRepository: CompanyRepository
 
+    @BeforeEach
+    fun setup() {
+        entityManager.clear()
+        entityManager.clear()
+        entityManager.flush()
+    }
+
     @Test
     fun `test findByName returns a company if it exists in DB`() {
         // mock
-        val name = "Google"
+        val name = "TestCompany"
         val expectedCompany = Company(name = name)
         entityManager.persist(expectedCompany)
         entityManager.flush()
@@ -37,7 +45,7 @@ internal class CompanyRepositoryTest {
     @Test
     fun `test findByName returns an empty optional if the company does not exist in DB`() {
         // mock
-        val name = "Google"
+        val name = "TestCompany"
 
         // action
         val actualCompanyOptional = companyRepository.findByName(name)
