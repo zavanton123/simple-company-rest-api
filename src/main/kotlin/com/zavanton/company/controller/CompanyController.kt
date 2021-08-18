216e49db532d197c0004944a3435756e1efec508
zavanton123 <zavanton123@gmail.com>
2021-08-18 12:08:09 +0300

    create company

--- src/main/kotlin/com/zavanton/company/controller/CompanyController.kt
+++ src/main/kotlin/com/zavanton/company/controller/CompanyController.kt
@@ -5,6 +5,9 @@ import com.zavanton.company.service.CompanyService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -20,5 +23,32 @@ class CompanyController(
         val companies: Set<Company> = companyService.fetchAllCompanies()
         model.addAttribute("companies", companies)
         return "companies/companies_list"
+    }
+
+    @GetMapping("/create")
+    fun showCreateCompanyForm(
+        model: Model
+    ): String {
+        val company = Company()
+        model.addAttribute("company", company)
+        return "companies/create_company"
+    }
+
+    @PostMapping("/process_create")
+    fun processCreateCompanyForm(
+        @ModelAttribute company: Company
+    ): String {
+        val savedCompany = companyService.createCompany(company)
+        return "redirect:/companies/${savedCompany.id}"
+    }
+
+    @GetMapping("/{id}")
+    fun showCompanyById(
+        @PathVariable("id") id: Long,
+        model: Model
+    ): String {
+        val company = companyService.fetchCompanyById(id)
+        model.addAttribute("company", company)
+        return "companies/company_details"
     }
 }
